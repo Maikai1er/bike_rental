7cccafe63e29d6ce7d5f506a9499e88ae36e8428
Maikai1er <kolesnichenko.misha12@gmail.com>
2024-07-10 14:17:26 +0200

    added models fot user, bike and rental

--- rentals/models.py
+++ rentals/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Bike(models.Model):
+    STATUS_CHOICES = (
+        ('rented', 'Rented'),
+        ('available', 'Available'),
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='available')
+
+
+class Rental(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bike = models.ForeignKey(Bike, on_delete=models.CASCADE)
+    start_time = models.DateField(auto_now_add=True)
+    end_time = models.DateField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
